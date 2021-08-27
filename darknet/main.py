09c114dcbf22be983ad4e08f0afc28c2d1df9ea3
voka <chdlswhd7@naver.com>
2021-08-27 16:25:07 +0900

    Yes

---
+++ darknet/main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import filedialog
+import tkinter as tk
+import subprocess
+import UI.UI as ui
+ 
+def main():
+    root = Tk()
+    UI = ui.UI(root)
+    UI.root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
